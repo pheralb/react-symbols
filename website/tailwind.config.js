f76d6dafe08efcae31aad79b657748139d0252ac
pheralb <pablohdez4340@gmail.com>
2022-09-12 21:39:05 +0100

    ⚒️ Tailwind css improvements.

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx,js,jsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
         mono: ['JetBrainsMono-Regular', 'monospace']
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        midnight: '#1E1E2E',
       }
-    }
+    },
   },
   plugins: [require('@tailwindcss/typography')]
 };
