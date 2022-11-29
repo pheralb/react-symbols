598bfb342c6cc65e6ef9c7612e0d9975aedb1880
pheralb <pablohdez4340@gmail.com>
2022-11-29 20:05:31 +0000

    ⚒️ Add folder icons.

---
+++ library/src/library/folders/folderRed.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps, FC } from 'react';
+
+const FolderRed: FC<ComponentProps<'svg'>> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="#64748B"
+      fillRule="evenodd"
+      d="M5 4a3 3 0 00-3 3v10a3 3 0 003 3h9v-2H5a1 1 0 01-1-1V7a1 1 0 011-1h2.784a1 1 0 01.635.227l2.393 1.966a3 3 0 001.904.682H19a1 1 0 011 1V10h2v-.125a3 3 0 00-3-3h-6.284a1 1 0 01-.635-.227L9.688 4.682A3 3 0 007.784 4H5z"
+      clipRule="evenodd"
+    ></path>
+    <circle cx="20" cy="16" r="4" fill="#F87171"></circle>
+  </svg>
+);
+
+export default FolderRed;
