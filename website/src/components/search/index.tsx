745c1163f5980ea0af55f12ab3b0717788c6d4d9
pheralb <pablohdez4340@gmail.com>
2023-06-07 10:59:54 +0100

    🎨 Search component improvements.

--- website/src/components/search/index.tsx
+++ website/src/components/search/index.tsx
@@ -1,11 +1,16 @@
 import { forwardRef } from "react"
+
 import SearchIcon from "../icons/search"
+import XIcon from "../icons/x"
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  clear?: boolean
+  clearAction?: () => void
+}
 
 const Search = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, clear, ...props }, ref) => {
     return (
       <div className="relative w-full">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 text-neutral-400">
@@ -20,6 +25,18 @@ const Search = forwardRef<HTMLInputElement, InputProps>(
           ref={ref}
           {...props}
         />
+        {clear && (
+          <div className="absolute inset-y-0 right-0 flex items-center pr-3 text-neutral-400">
+            <button
+              onClick={props.clearAction}
+              className="focus:ring-0.5 rounded-md border-neutral-700 p-1 hover:bg-neutral-700/10 focus:border focus:outline-none focus:ring-neutral-700"
+            >
+              <div className="pointer-events-none">
+                <XIcon width={25} />
+              </div>
+            </button>
+          </div>
+        )}
       </div>
     )
   },
