ac59db1685fd90f3b2514f918a38e99965b56dff
pheralb <pablohdez4340@gmail.com>
2023-10-18 17:00:20 +0100

    🚀 Upgrade to Remix v2.

--- website/remix.config.cjs
+++ website/remix.config.cjs
@@ -6,10 +6,4 @@ module.exports = {
   appDirectory: "src",
   tailwind: true,
   serverDependenciesToBundle: ["@react-symbols/icons"],
-  future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
 }
