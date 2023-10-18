5ba5de64b448a3b055f34ab65f2c1fd1ec7c7bcc
pheralb <pablohdez4340@gmail.com>
2023-10-18 23:24:38 +0100

    ✨ Design improvements.

--- website/src/components/card/index.tsx
+++ website/src/components/card/index.tsx
@@ -10,9 +10,16 @@ import {
 
 import useCopyToClipboard from "@/hooks/useCopyToClipboard"
 import Copy from "../icons/copy"
+import { Button, buttonVariants } from "../button"
+import Check from "../icons/check"
+import Code from "../icons/code"
+import ExternalLink from "../externalLink"
+import { lower } from "@/utils/lower"
 
+type Category = "folders" | "icon"
 interface CardProps {
   name: string
+  category: Category
   icon: ReactNode
 }
 
@@ -31,24 +38,58 @@ const Card = (props: CardProps) => {
   }
 
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger>
-          <div
-            className="bg-midgnight flex transform cursor-pointer flex-col items-center justify-center rounded-md border border-neutral-800 py-3 transition duration-200 ease-in-out hover:border-neutral-800 hover:bg-neutral-800/40"
-            onClick={() => copyToClipboard(`<${props.name} />`)}
-          >
-            {props.icon}
-            <p className="mt-2 font-medium text-gray-300">
-              {isCopied ? "Copied!" : props.name}
-            </p>
-          </div>
-        </TooltipTrigger>
-        <TooltipContent side="bottom">
-          <p>Copy "{props.name}" to clipboard</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <div className="bg-midgnight flex transform flex-col items-center justify-center rounded-md border border-neutral-800 py-3 transition duration-200 ease-in-out hover:border-neutral-800 hover:bg-neutral-800/40">
+      <div className="flex flex-col space-y-2 items-center justify-center mb-2">
+        {props.icon}
+        <p>{props.name}</p>
+      </div>
+      <div className="flex items-center">
+        <TooltipProvider delayDuration={0}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                onClick={() => copyToClipboard(`<${props.name} />`)}
+              >
+                {isCopied ? (
+                  <Check className="h-5 w-5 text-neutral-300" />
+                ) : (
+                  <Copy className="h-5 w-5 text-neutral-300" />
+                )}
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="bottom">
+              <p>Copy to clipboard</p>
+            </TooltipContent>
+          </Tooltip>
+          <Tooltip>
+            <TooltipTrigger>
+              <ExternalLink
+                href={
+                  props.category === "folders"
+                    ? `https://github.com/pheralb/react-symbols/blob/main/library/src/library/folders/${lower(
+                        props.name,
+                      )}.tsx`
+                    : `https://github.com/pheralb/react-symbols/blob/main/library/src/library/${lower(
+                        props.name,
+                      )}.tsx`
+                }
+                className={buttonVariants({
+                  variant: "ghost",
+                  size: "icon",
+                })}
+              >
+                <Code className="h-5 w-5 text-neutral-300" />
+              </ExternalLink>
+            </TooltipTrigger>
+            <TooltipContent side="bottom">
+              <p>Check code</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+    </div>
   )
 }
 
