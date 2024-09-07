c8871d83ef3d8c98285ef12bade53f6858ea73af
pheralb <pablohdez4340@gmail.com>
2024-09-08 00:09:53 +0100

    ✍️ Add new fonts

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -2,12 +2,17 @@ import type { Config } from "tailwindcss";
 
 // Plugins:
 import twAnimate from "tailwindcss-animate";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   darkMode: ["class"],
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+        mono: ["GeistMono", ...defaultTheme.fontFamily.mono],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
