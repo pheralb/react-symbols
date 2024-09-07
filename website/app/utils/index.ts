b6ecd72c25f2c7c97789be072df08cd179f1b4fa
pheralb <pablohdez4340@gmail.com>
2024-09-08 00:10:41 +0100

    ⚙️ Add ``lower`` utility

--- website/app/utils/index.ts
+++ website/app/utils/index.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const lower = (str: string) =>
+  str.charAt(0).toLowerCase() + str.slice(1);
