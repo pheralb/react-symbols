bfae88849fe7661222413dd36029eb9567657664
pheralb <pablohdez4340@gmail.com>
2024-09-08 12:28:03 +0100

    ⚙️ Add ``githubSrcRepository`` globals item

--- website/app/globals.ts
+++ website/app/globals.ts
@@ -1,7 +1,10 @@
 interface iGlobals {
   npmPackageName: string;
+  githubSrcRepository: string;
 }
 
 export const globals: iGlobals = {
   npmPackageName: "@react-symbols/icons",
+  githubSrcRepository:
+    "https://github.com/pheralb/react-symbols/blob/main/library/src/",
 };
