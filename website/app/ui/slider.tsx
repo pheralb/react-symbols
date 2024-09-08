1e70a0c6529a18b1aa74910887302b771f8b28ba
pheralb <pablohdez4340@gmail.com>
2024-09-08 14:45:42 +0100

    🎨 Update slider UI colors

--- website/app/ui/slider.tsx
+++ website/app/ui/slider.tsx
@@ -17,9 +17,9 @@ const Slider = forwardRef<
     {...props}
   >
     <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-zinc-900/20 dark:bg-zinc-800">
-      <SliderPrimitive.Range className="absolute h-full bg-zinc-900 dark:bg-zinc-700" />
+      <SliderPrimitive.Range className="absolute h-full bg-zinc-900 dark:bg-zinc-600" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-zinc-200 border-zinc-900/50 bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 dark:border-zinc-50/50 dark:border-zinc-800 dark:bg-zinc-600 dark:focus-visible:ring-zinc-600" />
+    <SliderPrimitive.Thumb className="block h-4 w-4 rounded-full border border-zinc-200 border-zinc-900/50 bg-white shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 dark:border-zinc-50/50 dark:border-zinc-800 dark:bg-zinc-500 dark:focus-visible:ring-zinc-600" />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
