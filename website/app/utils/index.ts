8eb3c39465b3225fe16146984030c5be20dfd877
pheralb <pablohdez4340@gmail.com>
2024-09-09 21:31:55 +0100

    ⚙️ Create ``clipboard`` utility

--- website/app/utils/index.ts
+++ website/app/utils/index.ts
@@ -7,3 +7,20 @@ export function cn(...inputs: ClassValue[]) {
 
 export const lower = (str: string) =>
   str.charAt(0).toLowerCase() + str.slice(1);
+
+const MIMETYPE = "text/plain";
+
+export const clipboard = async (content: string) => {
+  try {
+    const clipboardItem = new ClipboardItem({
+      [MIMETYPE]: new Blob([content], { type: MIMETYPE }),
+    });
+
+    setTimeout(async () => {
+      await navigator.clipboard.write([clipboardItem]);
+    }, 200);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    await navigator.clipboard.writeText(content);
+  }
+};
