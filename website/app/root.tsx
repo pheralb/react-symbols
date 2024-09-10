53b3851a8f093372c67a10dbf28872d8b97aae49
pheralb <pablohdez4340@gmail.com>
2024-09-10 23:52:04 +0100

    🔍 Add metatags

--- website/app/root.tsx
+++ website/app/root.tsx
@@ -95,6 +95,34 @@ export function Layout({ children }: { children: ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="keywords"
+          content="Symbols, VSCode, Icons, Theme, React, Miguel Solorio"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="React-Symbols" />
+        <meta
+          property="og:description"
+          content="Symbols VSCode Icons Theme by Miguel Solorio, for React."
+        />
+        <meta
+          name="image"
+          property="og:image"
+          content="https://react-symbols.vercel.app/images/og.png"
+        />
+        <meta property="og:image:width" content="1977" />
+        <meta property="og:image:height" content="1231" />
+        <meta property="og:url" content="https://react-symbols.vercel.app/" />
+        <meta property="og:site_name" content="React-Symbols" />
+        <meta name="author" content="@pheralb_" />
+        <meta
+          name="twitter:image"
+          content="https://react-symbols.vercel.app/images/og.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@pheralb_" />
+        <meta name="twitter:site" content="@pheralb_" />
+        <meta name="twitter:title" content="React-Symbols" />
         <Meta />
         <Links />
       </head>
