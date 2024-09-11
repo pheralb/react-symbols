b1252ca71fd5e0086360f275ffb085395d5d7993
pheralb <pablohdez4340@gmail.com>
2024-09-12 00:07:03 +0100

    ✨ Add ``BoxIcon`` feather icon

--- website/app/ui/icons/feather.tsx
+++ website/app/ui/icons/feather.tsx
@@ -110,3 +110,18 @@ export const DotIcon = (props: SVGProps<SVGSVGElement>) => (
     <circle cx={12} cy={12} r={3} />
   </svg>
 );
+
+export const BoxIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="m16.5 9.4-9-5.19M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" />
+    <path d="M3.27 6.96 12 12.01l8.73-5.05M12 22.08V12" />
+  </svg>
+);
