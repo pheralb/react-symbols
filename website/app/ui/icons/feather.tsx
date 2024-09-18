94524fb89ff7ae6b2fa633782741154a0592475c
pheralb <pablohdez4340@gmail.com>
2024-09-18 21:25:11 +0100

    🎨 Add new feather icons

--- website/app/ui/icons/feather.tsx
+++ website/app/ui/icons/feather.tsx
@@ -139,3 +139,17 @@ export const CrossIcon = (props: SVGProps<SVGSVGElement>) => (
     <path d="M18 6 6 18M6 6l12 12" />
   </svg>
 );
+
+export const AlertIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M10.29 3.86 1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0zM12 9v4M12 17h.01" />
+  </svg>
+);
