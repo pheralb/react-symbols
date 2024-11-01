564f14704780fb5591d3c9538a3d39245ce1a054
pheralb <pablohdez4340@gmail.com>
2024-11-01 11:42:48 +0000

    🎨 Delete ``active`` property for button

--- website/app/ui/button.tsx
+++ website/app/ui/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center space-x-2 justify-center whitespace-nowrap rounded-md text-sm font-medium transition-transform focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-200 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-zinc-700 active:scale-[0.97]",
+  "inline-flex items-center space-x-2 justify-center whitespace-nowrap rounded-md text-sm font-medium transition-transform focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-200 disabled:pointer-events-none disabled:opacity-50 dark:focus-visible:ring-zinc-700",
   {
     variants: {
       variant: {
