d1cc732347c307a58208fbb48dfe729a29af8187
pheralb <pablohdez4340@gmail.com>
2025-05-01 16:18:05 +0100

    🎨 Add v0 & shadcn/ui svgs + delete feather icons

--- website/app/ui/icons/svgl.tsx
+++ website/app/ui/icons/svgl.tsx
@@ -81,3 +81,40 @@ export const VSCode = (props: SVGProps<SVGSVGElement>) => (
     />
   </svg>
 );
+
+export const V0Icon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    height="1em"
+    xmlns="http://www.w3.org/2000/svg"
+    strokeLinejoin="round"
+    viewBox="0 0 16 16"
+    width="1em"
+    {...props}
+  >
+    <path
+      clipRule="evenodd"
+      d="M9.50321 5.5H13.2532C13.3123 5.5 13.3704 5.5041 13.4273 5.51203L9.51242 9.42692C9.50424 9.36912 9.5 9.31006 9.5 9.25L9.5 5.5L8 5.5L8 9.25C8 10.7688 9.23122 12 10.75 12H14.5V10.5L10.75 10.5C10.6899 10.5 10.6309 10.4958 10.5731 10.4876L14.4904 6.57028C14.4988 6.62897 14.5032 6.68897 14.5032 6.75V10.5H16.0032V6.75C16.0032 5.23122 14.772 4 13.2532 4H9.50321V5.5ZM0 5V5.00405L5.12525 11.5307C5.74119 12.3151 7.00106 11.8795 7.00106 10.8822V5H5.50106V9.58056L1.90404 5H0Z"
+      fill="currentColor"
+      fillRule="evenodd"
+    />
+  </svg>
+);
+
+export const Shadcnui = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 256 256"
+    width="1em"
+    height="1em"
+    {...props}
+  >
+    <path fill="none" d="M0 0h256v256H0z" />
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={25}
+      strokeLinecap="round"
+      d="M208 128l-80 80M192 40L40 192"
+    />
+  </svg>
+);
