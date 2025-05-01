ebcdc0b0364ae8fa34d52048832c88d7a0ae92b3
pheralb <pablohdez4340@gmail.com>
2025-05-01 17:59:55 +0100

    🎨 Improve ThemeSwitcher UI component, fixed change mode

--- website/app/components/themeSwitcher.tsx
+++ website/app/components/themeSwitcher.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/ui/button";
+import { cn } from "@/utils";
 import { MoonIcon, SunIcon } from "lucide-react";
 import { Theme, useTheme } from "remix-themes";
 
 const ThemeSwitcher = () => {
-  const [, setTheme] = useTheme();
+  const [theme, setTheme] = useTheme();
   const iconSize = 14;
 
   const handleChangeTheme = (theme: Theme) => {
@@ -13,17 +14,25 @@ const ThemeSwitcher = () => {
   return (
     <span className="flex w-fit items-center overflow-hidden rounded-md border border-zinc-200 bg-white dark:border-zinc-800 dark:bg-zinc-800/40">
       <Button
+        title="Light mode"
         variant="ghost"
         size="sm"
-        className="rounded-none border-r border-zinc-200 px-2.5 dark:border-zinc-800"
+        className={cn(
+          "rounded-none border-r border-zinc-200 px-2.5 dark:border-zinc-800",
+          Theme.LIGHT === theme && "bg-zinc-200",
+        )}
         onClick={() => handleChangeTheme(Theme.LIGHT)}
       >
         <SunIcon size={iconSize} />
       </Button>
       <Button
+        title="Dark mode"
         variant="ghost"
         size="sm"
-        className="rounded-none px-2.5"
+        className={cn(
+          "rounded-none px-2.5",
+          Theme.DARK === theme && "dark:bg-zinc-800",
+        )}
         onClick={() => handleChangeTheme(Theme.DARK)}
       >
         <MoonIcon size={iconSize} />
