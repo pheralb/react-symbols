e66e12d9b99a94743479a72c27344dfe301b8a1f
pheralb <pablohdez4340@gmail.com>
2025-05-01 16:22:16 +0100

    ⚙️ Fixed Sonner styles

--- website/app/providers/sonner.tsx
+++ website/app/providers/sonner.tsx
@@ -1,20 +1,26 @@
+import { useTheme } from "@/theme/themeProvider";
 import { Toaster as Sonner } from "sonner";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
+  const [theme] = useTheme();
   return (
     <Sonner
-      theme="dark"
-      className="toaster group font-sans"
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group"
       position="bottom-center"
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-zinc-100 dark:group-[.toaster]:bg-zinc-900 group-[.toaster]:text-zinc-950 dark:group-[.toaster]:text-zinc-50 group-[.toaster]:border-zinc-200 dark:group-[.toaster]:border-zinc-800 group-[.toaster]:shadow-lg",
+            "group toast font-sans group-[.toaster]:bg-white group-[.toaster]:text-zinc-900 group-[.toaster]:border-zinc-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:bg-zinc-900 dark:group-[.toaster]:text-zinc-50 dark:group-[.toaster]:border-zinc-800",
           description:
-            "group-[.toaster]:text-zinc-600 dark:group-[.toaster]:text-zinc-400 font-mono",
-          icon: "group-[.toaster]:mr-3",
+            "group-[.toast]:text-zinc-500 dark:group-[.toast]:text-zinc-400",
+          actionButton:
+            "group-[.toast]:bg-zinc-900 group-[.toast]:text-zinc-50 dark:group-[.toast]:bg-zinc-50 dark:group-[.toast]:text-zinc-900",
+          cancelButton:
+            "group-[.toast]:bg-zinc-100 group-[.toast]:text-zinc-500 dark:group-[.toast]:bg-zinc-800 dark:group-[.toast]:text-zinc-400",
+          content: "group-[.toaster]:ml-2",
         },
       }}
       {...props}
