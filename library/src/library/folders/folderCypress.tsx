2d4bb5a7192cf19438b7d048bdbdc6d0021bb6aa
pheralb <pablohdez4340@gmail.com>
2025-05-04 16:16:47 +0100

    📦 Add new folder icons

---
+++ library/src/library/folders/folderCypress.tsx
@@ -0,0 +1,97 @@
+import type { ComponentProps, FC } from "react";
+
+const FolderCypress: FC<ComponentProps<"svg">> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="#64748B"
+      fillRule="evenodd"
+      d="M5 4a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h8v-2H5a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h2.784a1 1 0 0 1 .635.227l2.393 1.966a3 3 0 0 0 1.904.682H19a1 1 0 0 1 1 1V10h2v-.125a3 3 0 0 0-3-3h-6.284a1 1 0 0 1-.635-.227L9.688 4.682A3 3 0 0 0 7.784 4z"
+      clipRule="evenodd"
+    ></path>
+    <g clipPath="url(#clip0_4_143)">
+      <path
+        fill="#69D3A7"
+        d="M16.514 13.969c.405 0 .733.216.901.59l.013.03.678-.23-.014-.035c-.262-.641-.867-1.039-1.578-1.039-.5 0-.906.16-1.242.49-.333.328-.502.74-.502 1.226 0 .481.17.891.502 1.218.336.33.742.49 1.242.49.71 0 1.315-.398 1.578-1.038l.014-.035-.68-.23-.01.03a.94.94 0 0 1-.902.59.95.95 0 0 1-.706-.292 1.02 1.02 0 0 1-.286-.733c0-.292.094-.535.286-.74a.95.95 0 0 1 .706-.292"
+      ></path>
+      <path
+        fill="#69D3A7"
+        fillOpacity="0.08"
+        d="M16.514 13.969c.405 0 .733.216.901.59l.013.03.678-.23-.014-.035c-.262-.641-.867-1.039-1.578-1.039-.5 0-.906.16-1.242.49-.333.328-.502.74-.502 1.226 0 .481.17.891.502 1.218.336.33.742.49 1.242.49.71 0 1.315-.398 1.578-1.038l.014-.035-.68-.23-.01.03a.94.94 0 0 1-.902.59.95.95 0 0 1-.706-.292 1.02 1.02 0 0 1-.286-.733c0-.292.094-.535.286-.74a.95.95 0 0 1 .706-.292"
+      ></path>
+      <path
+        fill="url(#paint0_linear_4_143)"
+        d="m19.457 19.784-.202-.668a4.28 4.28 0 0 0 3.048-4.115c0-.526-.093-1.041-.279-1.528l.65-.248c.216.567.326 1.164.326 1.777a4.97 4.97 0 0 1-3.543 4.781"
+      ></path>
+      <path
+        fill="#69D3A7"
+        d="M22.115 13.74A4.28 4.28 0 0 0 18 10.698q-.39 0-.771.069l-.123-.686c.292-.053.593-.08.893-.08a4.97 4.97 0 0 1 4.781 3.533z"
+      ></path>
+      <path
+        fill="url(#paint1_linear_4_143)"
+        d="M13.32 16.763A5 5 0 0 1 13 15a4.98 4.98 0 0 1 4.503-4.974l.068.693a4.286 4.286 0 0 0-3.875 4.282c0 .521.092 1.033.274 1.516z"
+      ></path>
+      <path
+        fill="#69D3A7"
+        d="m20.76 13.349-.966 2.45-.973-2.45h-.795l1.364 3.343-.994 2.415.696.139 2.419-5.897z"
+      ></path>
+      <path
+        fill="#69D3A7"
+        fillOpacity="0.08"
+        d="m20.76 13.349-.966 2.45-.973-2.45h-.795l1.364 3.343-.994 2.415.696.139 2.419-5.897z"
+      ></path>
+      <path
+        fill="url(#paint2_linear_4_143)"
+        d="m18.522 18.803-.147.355a.22.22 0 0 1-.197.14q-.09.005-.18.005a4.32 4.32 0 0 1-4.136-3.115l-.67.191A5.02 5.02 0 0 0 17.998 20h.001q.105.001.209-.004a.92.92 0 0 0 .812-.571l.198-.482z"
+      ></path>
+    </g>
+    <defs>
+      <linearGradient
+        id="paint0_linear_4_143"
+        x1="23.383"
+        x2="18.915"
+        y1="15.032"
+        y2="17.921"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.081" stopColor="#69D3A7"></stop>
+        <stop offset="1" stopColor="#69D3A7" stopOpacity="0"></stop>
+      </linearGradient>
+      <linearGradient
+        id="paint1_linear_4_143"
+        x1="16.335"
+        x2="13.524"
+        y1="10.064"
+        y2="16.018"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#69D3A7"></stop>
+        <stop offset="0.823" stopColor="#2AB586"></stop>
+        <stop offset="1" stopColor="#1CAE7F"></stop>
+      </linearGradient>
+      <linearGradient
+        id="paint2_linear_4_143"
+        x1="12.993"
+        x2="20.349"
+        y1="17.29"
+        y2="19.595"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.077" stopColor="#1CAE7F"></stop>
+        <stop offset="0.164" stopColor="#1CA379"></stop>
+        <stop offset="0.316" stopColor="#1C8568"></stop>
+        <stop offset="0.516" stopColor="#1B554D"></stop>
+        <stop offset="0.719" stopColor="#1B1E2E"></stop>
+      </linearGradient>
+      <clipPath id="clip0_4_143">
+        <path fill="#fff" d="M13 10h10v10H13z"></path>
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default FolderCypress;
---
+++ library/src/library/folders/folderVSCode.tsx
@@ -0,0 +1,132 @@
+import type { ComponentProps, FC } from "react";
+
+const FolderVSCode: FC<ComponentProps<"svg">> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      fill="#64748B"
+      fillRule="evenodd"
+      d="M5 4a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h8v-2H5a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h2.784a1 1 0 0 1 .635.227l2.393 1.966a3 3 0 0 0 1.904.682H19a1 1 0 0 1 1 1V10h2v-.125a3 3 0 0 0-3-3h-6.284a1 1 0 0 1-.635-.227L9.688 4.682A3 3 0 0 0 7.784 4z"
+      clipRule="evenodd"
+    ></path>
+    <mask
+      id="mask0_38272_281"
+      width="10"
+      height="10"
+      x="14"
+      y="11"
+      maskUnits="userSpaceOnUse"
+      style={{ maskType: "alpha" }}
+    >
+      <path
+        fill="#fff"
+        fillRule="evenodd"
+        d="M21.091 20.932a.62.62 0 0 0 .496-.02l2.06-.99a.63.63 0 0 0 .353-.563V12.64c0-.24-.138-.459-.354-.563l-2.059-.99a.62.62 0 0 0-.71.12l-3.941 3.596-1.717-1.303a.416.416 0 0 0-.532.024l-.55.5a.417.417 0 0 0-.001.617L15.625 16l-1.49 1.358a.417.417 0 0 0 .001.616l.55.501a.416.416 0 0 0 .533.024l1.717-1.303 3.94 3.596q.096.093.215.14m.41-7.202L18.511 16l2.99 2.27z"
+        clipRule="evenodd"
+      ></path>
+    </mask>
+    <g mask="url(#mask0_38272_281)">
+      <path
+        fill="#0065A9"
+        d="m23.646 12.08-2.06-.992a.62.62 0 0 0-.711.12l-6.745 6.15a.417.417 0 0 0 0 .616l.551.501a.42.42 0 0 0 .532.024l8.123-6.162a.414.414 0 0 1 .664.33v-.024a.63.63 0 0 0-.354-.563"
+      ></path>
+      <g filter="url(#filter0_d_38272_281)">
+        <path
+          fill="#007ACC"
+          d="m23.646 19.92-2.06.992a.62.62 0 0 1-.711-.12l-6.745-6.15a.417.417 0 0 1 0-.617l.551-.5a.42.42 0 0 1 .532-.024l8.123 6.162a.414.414 0 0 0 .664-.33v.024c0 .24-.137.46-.354.563"
+        ></path>
+      </g>
+      <g filter="url(#filter1_d_38272_281)">
+        <path
+          fill="#1F9CF0"
+          d="M21.586 20.913a.62.62 0 0 1-.711-.121c.23.23.625.067.625-.26v-9.065a.366.366 0 0 0-.625-.259.62.62 0 0 1 .71-.12l2.06.99a.63.63 0 0 1 .355.563v6.718c0 .24-.138.459-.354.563z"
+        ></path>
+      </g>
+      <path
+        fill="url(#paint0_linear_38272_281)"
+        fillRule="evenodd"
+        d="M21.085 20.932a.62.62 0 0 0 .496-.02l2.059-.99a.63.63 0 0 0 .354-.563V12.64c0-.24-.138-.459-.354-.563l-2.059-.99a.62.62 0 0 0-.71.12l-3.942 3.596-1.716-1.303a.416.416 0 0 0-.532.024l-.55.5a.417.417 0 0 0-.001.617L15.619 16l-1.49 1.358a.417.417 0 0 0 .001.617l.55.5a.416.416 0 0 0 .533.024l1.716-1.303 3.942 3.596q.094.094.214.14m.41-7.202L18.505 16l2.99 2.27z"
+        clipRule="evenodd"
+        opacity="0.25"
+        style={{ mixBlendMode: "overlay" }}
+      ></path>
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_38272_281"
+        width="11.673"
+        height="9.225"
+        x="13.161"
+        y="12.583"
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+        <feColorMatrix
+          in="SourceAlpha"
+          result="hardAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        ></feColorMatrix>
+        <feOffset></feOffset>
+        <feGaussianBlur stdDeviation="0.417"></feGaussianBlur>
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"></feColorMatrix>
+        <feBlend
+          in2="BackgroundImageFix"
+          mode="overlay"
+          result="effect1_dropShadow_38272_281"
+        ></feBlend>
+        <feBlend
+          in="SourceGraphic"
+          in2="effect1_dropShadow_38272_281"
+          result="shape"
+        ></feBlend>
+      </filter>
+      <filter
+        id="filter1_d_38272_281"
+        width="4.792"
+        height="11.615"
+        x="20.042"
+        y="10.192"
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix"></feFlood>
+        <feColorMatrix
+          in="SourceAlpha"
+          result="hardAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        ></feColorMatrix>
+        <feOffset></feOffset>
+        <feGaussianBlur stdDeviation="0.417"></feGaussianBlur>
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"></feColorMatrix>
+        <feBlend
+          in2="BackgroundImageFix"
+          mode="overlay"
+          result="effect1_dropShadow_38272_281"
+        ></feBlend>
+        <feBlend
+          in="SourceGraphic"
+          in2="effect1_dropShadow_38272_281"
+          result="shape"
+        ></feBlend>
+      </filter>
+      <linearGradient
+        id="paint0_linear_38272_281"
+        x1="18.994"
+        x2="18.994"
+        y1="11.026"
+        y2="20.974"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#fff"></stop>
+        <stop offset="1" stopColor="#fff" stopOpacity="0"></stop>
+      </linearGradient>
+    </defs>
+  </svg>
+);
+
+export default FolderVSCode;
