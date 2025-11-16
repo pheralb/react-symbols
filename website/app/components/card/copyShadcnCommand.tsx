00204c4f31134ef1f97f2cd2147f200ebf76ee92
pheralb <pablohdez4340@gmail.com>
2025-11-16 23:04:33 +0000

    🎨 Separate copy utilities + improve ``copy library``

---
+++ website/app/components/card/copyShadcnCommand.tsx
@@ -0,0 +1,46 @@
+import type { FC, SVGProps } from "react";
+
+import { clipboard } from "@/utils";
+import { toast } from "@pheralb/toast";
+import { appConfig } from "@/config";
+import { Button } from "@/ui/button";
+import { Shadcnui } from "@/ui/icons/svgl";
+
+interface CopyShadcnCommandProps {
+  itemName: string;
+  iconSize: number;
+  isFolder?: boolean;
+  ItemIcon: FC<SVGProps<SVGSVGElement>>;
+}
+
+const CopyShadcnCommand = ({
+  itemName,
+  iconSize,
+  isFolder,
+  ItemIcon,
+}: CopyShadcnCommandProps) => {
+  const handleCopyShadcnCommand = async () => {
+    const itemNameLower = itemName.charAt(0).toLowerCase() + itemName.slice(1);
+    const makeCommand = !isFolder
+      ? `${appConfig.shadcnCommand} ${appConfig.registryUrl}${itemNameLower}.json`
+      : `${appConfig.shadcnCommand} ${appConfig.registryUrl}folders/${itemNameLower}.json`;
+
+    await clipboard(makeCommand);
+    toast.success({
+      text: "Copied shadcn/ui command",
+      icon: <ItemIcon width={24} height={24} />,
+    });
+  };
+  return (
+    <Button
+      title="Copy shadcn/ui command"
+      variant="ghost"
+      size="icon"
+      onClick={handleCopyShadcnCommand}
+    >
+      <Shadcnui width={iconSize} height={iconSize} />
+    </Button>
+  );
+};
+
+export default CopyShadcnCommand;
