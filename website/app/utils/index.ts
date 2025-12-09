f0d1b5ef8dc35a66c587b3c894d417c8c8d75620
pheralb <pablohdez4340@gmail.com>
2025-12-09 23:32:46 +0000

    feat: add text utility functions

--- website/app/utils/index.ts
+++ website/app/utils/index.ts
@@ -21,3 +21,19 @@ export const clipboard = async (content: string) => {
     await navigator.clipboard.writeText(content);
   }
 };
+
+export const pascalToTitle = (str: string): string => {
+  return str
+    .replace(/([A-Z])/g, " $1")
+    .trim()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+export const titleToPascal = (str: string): string => {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toLowerCase() + word.slice(1))
+    .join("");
+};
