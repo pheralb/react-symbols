99bcc641b151981781ef122f85211feff8c5858f
pheralb <pablohdez4340@gmail.com>
2026-03-13 21:02:10 +0100

    fix: update padding for Grid component in Index and Folders routes

--- website/app/routes/_index.tsx
+++ website/app/routes/_index.tsx
@@ -39,7 +39,7 @@ export default function Index() {
       {filteredIcons.length === 0 ? (
         <NotFound input={search} />
       ) : (
-        <Grid className={cn(containerClasses, "pt-6 pb-14")}>
+        <Grid className={cn(containerClasses, "pt-6 pb-16")}>
           {filteredIcons.map((icon) => {
             return (
               <Card
