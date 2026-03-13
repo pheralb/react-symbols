410620402b91124ce90757942f149965578a80bf
pheralb <pablohdez4340@gmail.com>
2026-03-13 15:19:19 +0100

    feat: (library) refactor Vite and Vitest components to include new SVG implementations

--- library/src/library/vite.tsx
+++ library/src/library/vite.tsx
@@ -1,6 +1,6 @@
 import type { ComponentProps, FC } from "react";
 
-const Vite: FC<ComponentProps<"svg">> = (props) => (
+const Vite_Old: FC<ComponentProps<"svg">> = (props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
@@ -18,4 +18,20 @@ const Vite: FC<ComponentProps<"svg">> = (props) => (
   </svg>
 );
 
-export default Vite;
+const Vite: FC<ComponentProps<"svg">> = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="-4 -4 56 54"
+    {...props}
+  >
+    <path
+      fill="#8B5CF6"
+      d="M25.946 44.938c-.664.845-2.021.375-2.021-.698V33.937a2.26 2.26 0 0 0-2.262-2.262H10.287c-.92 0-1.456-1.04-.92-1.788l7.48-10.471c1.07-1.497 0-3.578-1.842-3.578H1.237c-.92 0-1.456-1.04-.92-1.788L10.013.474c.214-.297.556-.474.92-.474h28.894c.92 0 1.456 1.04.92 1.788l-7.48 10.471c-1.07 1.498 0 3.579 1.842 3.579h11.377c.943 0 1.473 1.088.89 1.83L25.947 44.94z"
+    ></path>
+  </svg>
+);
+
+export { Vite, Vite_Old };
